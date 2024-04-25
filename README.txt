2330b501cd6c2f7041553bc87e53c893ec0502df
Paul Goins <general@vultaire.net>
2024-04-24 22:02:59 -0700

    Initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+This is a super simple Tampermonkey script that I created to make YouTube
+videos larger when using LingQ in a browser.  I'm sharing this quickly in case
+it is useful to others, but with NO WARRANTY.
+
+This is a very trivial script and I am releasing it into the public domain.
+Use it as you will.  However, I am providing no support for it, and again,
+there is no warranty on this whatsoever.
---
+++ userscript.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         LingQ Video resizer
+// @namespace    http://vultaire.net/
+// @version      2024-04-25
+// @match        https://www.lingq.com/en/learn/*/web/reader/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Edit these as you like.
+    var height = 370;
+    var width = 600;
+
+    // This will inject the custom style.
+    var style = document.createElement('style')
+    style.textContent = `
+div.video-wrapper {
+  height: ${height}px !important;
+  width: ${width}px !important;
+}
+
+div.modal-content.is-medium {
+  max-width: ${width}px !important;
+}
+`
+    document.querySelector('head').appendChild(style)
+})();
